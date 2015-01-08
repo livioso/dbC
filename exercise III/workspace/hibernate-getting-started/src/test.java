9fb33e9b08de1b06e0b849df30f11ae7831255f3
Livio Bieri <livio@livio.li>
2015-01-08 15:21:35 +0100

    Deprecation issues fixed

--- exercise III/workspace/hibernate-getting-started/src/test.java
+++ exercise III/workspace/hibernate-getting-started/src/test.java
@@ -1,24 +1,27 @@
 
-import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.service.ServiceRegistry;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
 
 
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		AnnotationConfiguration config = new AnnotationConfiguration();
+		Configuration config = new Configuration();
 		config.addAnnotatedClass(College.class);
 		config.addAnnotatedClass(Student.class);
 		config.configure("hibernate.cfg.xml");
 		
-		
 		new SchemaExport(config).create(true,true);
 		
-		SessionFactory factory = config.buildSessionFactory();
+		ServiceRegistry serviceRegistry =
+			new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
+		
+		SessionFactory factory = config.buildSessionFactory(serviceRegistry);
 		Session session = factory.getCurrentSession();
 		session.beginTransaction();
 		
